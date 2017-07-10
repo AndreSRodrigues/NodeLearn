38b42c3fc6a8541e92f9dea26fd5973e44c4f559
Matheus Moscardini <matheus_moscardini@yahoo.com.br>
2017-07-10 14:05:07 -0300

    First Upload

---
+++ app.js
@@ -0,0 +1,87 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require ('body-parser');
+const path = require ('path');
+const expressValidator = require('express-validator');
+const util = require('util');
+
+//Inicializa a aplicação em uma variavel
+var app = express();
+
+//Setup viwe engine. Para renderizar a view pelo navegador
+app.set ('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Middleware do bodyParser. Explicado na documentação ao móulo
+//Serve para lidar com a leitura e envio de arquivos json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Chamando a pasta com conteúdos estáticos. 
+//Acessar pasta com conteúdos estáticos. Css, Jquery, etc
+// Não utilizado por enquanto
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Global Variables
+app.use (function(req, res, next){
+	res.locals.errors = null;
+	next();
+});
+
+//Middleware para validação do formulário
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//Get o request da página (/) 
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.post('/users/add', (req, res) =>{
+
+	req.checkBody('name', 'Porfavor, preencha seu nome').notEmpty();
+	req.checkBody('email', 'Email invalido').notEmpty();
+
+	//Verificar o resultado da operação
+	req.getValidationResult().then((result)=>{
+		//PAREI AQUI!! CONTINUAR AQUI!!!
+		//Ainda falta descobrir como quebrar essa string em mensagens utilizáveis
+		console.log(result.array());
+		errors = result.array();
+		//Se o resultado tiver algo, ou seja, um erro
+		if (!result.isEmpty()){
+			//Tratar o erro e retornar
+      		res.render('index', {
+      			errors: errors
+      		});
+			return;
+		}
+		//Se não tiver erro, prosseguir
+		var newUser = {
+			first_name: req.body.name,
+			email: req.body.email
+		}
+		console.log (newUser);
+	});
+	
+});
+
+app.listen(3000, () => {
+	console.log ('Server started of port 3000');
+});
+
+
